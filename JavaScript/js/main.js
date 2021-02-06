99e9a1f4f49e716a558abde2aeb535352d433c25
matheusnleite <matheusn06leite@gmail.com>
2021-02-06 10:06:29 -0300

    commit de 2021

---
+++ JavaScript/js/main.js
@@ -0,0 +1,48 @@
+
+/* var nome = "Matheus N Leite";
+ var idade = 14;
+ var idade2 = 10;
+ var frase = "Japão é o melhor time do mundo";
+ 
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"));*/
+
+//var lista = ["maça", "pêra", "laranja" ];
+//lista.pop();
+//lista.push("uva");
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var fruta = {nome: "maça", cor:"vermelha"};
+//console.log(fruta); 
+
+/*var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}];
+console.log(frutas); 
+alert(frutas[1].nome);*/
+
+/*var idade = prompt("Qual sua iadade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}*/
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count = count + 1;
+}*/
+
+/*var count;
+for(count =0; count <=5; count ++){
+    alert(count);
+}*/
+
+var d = new Date();
+alert(d.getMonth()+1);
